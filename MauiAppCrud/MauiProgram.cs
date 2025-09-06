3d238c2f562c5262804621cae59c0a817655bca8
Mateus Vinícius Miotto <mateusbpk@gmail.com>
2025-09-05 21:04:17 -0300

    FEAT: Full Screen Inicialization and Button Icons (save and delete)

--- MauiAppCrud/MauiProgram.cs
+++ MauiAppCrud/MauiProgram.cs
@@ -1,8 +1,12 @@
 ﻿using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
 using Syncfusion.Maui.Toolkit.Hosting;
-using Windows.UI.Notifications;
 
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+#endif
 namespace MauiAppCrud
 {
     public static class MauiProgram
@@ -14,6 +18,23 @@ namespace MauiAppCrud
                 .UseMauiApp<App>()
                 .UseMauiCommunityToolkit()
                 .ConfigureSyncfusionToolkit()
+                // Add this method for builder to set fullscreen on windows.
+                .ConfigureLifecycleEvents(events =>
+                {
+#if WINDOWS
+                    events.AddWindows(w =>
+                    {
+                        w.OnWindowCreated(window =>
+                        {
+                            window.ExtendsContentIntoTitleBar = true; //If you need to completely hide the minimized maximized close button, you need to set this value to false.
+                            IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                            WindowId myWndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+                            var _appWindow = AppWindow.GetFromWindowId(myWndId);
+                            _appWindow.SetPresenter(AppWindowPresenterKind.FullScreen);
+                        });
+                    });
+#endif
+                })
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
